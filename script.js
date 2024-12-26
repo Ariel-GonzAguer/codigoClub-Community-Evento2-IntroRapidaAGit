a8c274b0181461343741b15bb43fc86879361082
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2024-12-26 16:50:19 -0600

    inicio script2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const boton = document.getElementById("botonJS");
 
 function sayHello() {
-  console.log('hola desde acá');
+  console.log('😎🏍️');
 }
 
 boton.addEventListener('click', () => {
-  alert('nada');
+  alert('Holas');
   sayHello();
+  console.log('☕☕☕');
   document.body.style.backgroundColor = 'teal';
   document.body.style.color = 'black';
 });
