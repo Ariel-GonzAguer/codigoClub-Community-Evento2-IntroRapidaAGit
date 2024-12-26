7c873384b7f7cd3934deb45a7413fb13a0f9292e
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2024-12-26 16:46:26 -0600

    actualizar función

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const boton = document.getElementById("botonJS");
 
 function sayHello() {
-  console.log("Hola desde acá ⌨️");
+  console.log('😎🏍️');
 }
 
-boton.addEventListener("click", () => {
-  alert("nada");
+boton.addEventListener('click', () => {
+  alert('Holas ✌🏼');
   sayHello();
-  document.body.style.backgroundColor = "teal";
-  document.body.style.color = "black";
+  document.body.style.backgroundColor = 'teal';
+  document.body.style.color = 'black';
 });
